6809e6a1e14bc35b7fcd9459c93e4fb9735763b9
Andrew Henrique <andrewhenriquef@gmail.com>
2017-11-19 17:47:35 -0200

    Implemented supplier register and authentication

---
+++ src/controller/BeanSupplier.java
@@ -0,0 +1,114 @@
+package controller;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import model.Supplier;
+import repository.RepositorySupplier;
+
+@ManagedBean
+public class BeanSupplier {
+
+	private String email;
+	private String password;
+	private String cnpj;
+	private String name;
+	private String phone;
+	private String address;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String auth() {
+		FacesContext fc = FacesContext.getCurrentInstance();
+		EntityManager manager = getEntityManager();
+		RepositorySupplier repository =	new RepositorySupplier(manager);
+		if(repository.validate(email, password)) {
+			ExternalContext ec = fc.getExternalContext();
+			HttpSession session = (HttpSession) ec.getSession(false);
+			session.setAttribute("user", email);
+			return "/home";
+		} else {
+			FacesMessage fm = new FacesMessage("login e/ou senha invalidos");
+			fm.setSeverity(FacesMessage.SEVERITY_ERROR);
+			fc.addMessage(null, fm);
+			return "/supplier/supplier-login";
+		}
+	}
+	
+	public String register(){
+		FacesContext fc = FacesContext.getCurrentInstance();
+		EntityManager manager = getEntityManager();
+		RepositorySupplier repository = new RepositorySupplier(manager);
+		
+		Supplier supplier = new Supplier();
+		
+		supplier.setEmail(email);
+		supplier.setPassword(password);
+		supplier.setCnpj(cnpj);
+		supplier.setName(name);
+		supplier.setPhone(phone);
+		supplier.setAddress(address);
+		
+		repository.insert(supplier);
+		return "/supplier/supplier-login";
+	}
+	
+	public String loggout() {
+		FacesContext fc = FacesContext.getCurrentInstance();
+		ExternalContext ec = fc.getExternalContext();
+		HttpSession session = (HttpSession) ec.getSession(false);
+		session.removeAttribute("user");
+		session.invalidate();
+		return "/index.xhtml";
+	}
+	
+	private EntityManager getEntityManager() {
+		FacesContext fc = FacesContext.getCurrentInstance();
+		ExternalContext ec = fc.getExternalContext();
+		HttpServletRequest request = (HttpServletRequest) ec.getRequest();
+		EntityManager manager = (EntityManager)request.getAttribute("EntityManager");
+		return manager;
+	}
+
+
+}
---
+++ src/repository/RepositorySupplier.java
@@ -0,0 +1,32 @@
+package repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
+import model.Supplier;
+
+public class RepositorySupplier {
+
+	private EntityManager manager;
+	
+	public RepositorySupplier(EntityManager manager) {
+		this.manager = manager;
+	}
+	
+	public void insert(Supplier supplier) {
+		manager.persist(supplier);
+	}
+	
+	public boolean validate(String email, String password) {
+		Query query = manager.createQuery(
+				"select c from Supplier c where c.email = ?1 and c.password = ?2");
+		query.setParameter(1, email).setParameter(2, password);
+		try {
+			query.getSingleResult();
+			return true;
+		} catch (NoResultException e) {
+			return false;
+		}
+	}
+}
