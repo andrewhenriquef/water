2c53e9fd3f2a671993a1498e53fa1d086197059d
Andrew Henrique <andrewhenriquef@gmail.com>
2017-12-03 22:07:02 -0200

    Create Water files

---
+++ src/controller/BeanWater.java
@@ -0,0 +1,28 @@
+package controller;
+
+public class BeanWater {
+	private int id;
+	private int total_litters;
+	private int used_litters;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getTotal_litters() {
+		return total_litters;
+	}
+	public void setTotal_litters(int total_litters) {
+		this.total_litters = total_litters;
+	}
+	public int getUsed_litters() {
+		return used_litters;
+	}
+	public void setUsed_litters(int used_litters) {
+		this.used_litters = used_litters;
+	}
+	
+
+}
